8a299c3f8a92543a1546414cde81245d66cc761d
andreazanottoits <andrea.zanotto@stud.tecnicosuperiorekennedy.it>
2021-12-02 17:50:16 +0100

    primo commit

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,19 @@
+﻿using Gruppo4MicroserviziDTO.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("orders")]
+    public class OrdersController : Controller
+    {
+
+
+        [HttpPost]
+        public StatusCodeResult CreateOrder([FromBody] NewOrderEvent input)
+        {
+            Console.Write(input.IdCliente);
+            return StatusCode(201);
+        }
+    }
+}
