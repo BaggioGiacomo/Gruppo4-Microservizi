2681b90d26414f52efd2728eeacd7c259a266437
paolopiaia <83641900+paolopiaia@users.noreply.github.com>
2021-12-10 16:46:26 +0100

    Create new models because we need the same props name as the table columns name

---
+++ Gruppo4.Microservizi.AppCore/Models/ModelContrib/OrderContrib.cs
@@ -0,0 +1,33 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppo4.Microservizi.AppCore.Models.ModelContrib
+{
+    [Table("Orders")]
+    public class OrderContrib
+    {
+        public OrderContrib(string id, int customerId, decimal totalPrice, decimal discountAmount, decimal discountedPrice)
+        {
+            Id = Guid.Parse(id);
+            Customer_Id = customerId;
+            TotalPrice = totalPrice;
+            DiscountAmount = discountAmount;
+            DiscountedPrice = discountedPrice;
+        }
+        public OrderContrib()
+        {
+            Id = new Guid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+        public int Customer_Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal DiscountedPrice { get; set; }
+    }
+}
