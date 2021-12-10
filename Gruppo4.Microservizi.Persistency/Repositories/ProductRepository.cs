7554f99a155a33ea8e140ffe0a5629559accc124
paolopiaia <83641900+paolopiaia@users.noreply.github.com>
2021-12-10 12:12:27 +0100

    Create ProductRepository.cs

---
+++ Gruppo4.Microservizi.Persistency/Repositories/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppo4.Microservizi.Persistency.Repositories
+{
+    public class ProductRepository
+    {
+    }
+}
