1e6aeee9b3374ccd3b0d42aa32a951bc757c033a
paolopiaia <83641900+paolopiaia@users.noreply.github.com>
2021-12-13 16:49:43 +0100

    Create repositories interfaces

---
+++ Gruppo4.Microservizi.AppCore/Interfaces/Data/IOrdersHasProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Gruppo4.Microservizi.AppCore.Models;
+using Gruppo4.Microservizi.AppCore.Models.ModelContrib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppo4.Microservizi.AppCore.Interfaces.Data
+{
+    public interface IOrdersHasProductRepository
+    {
+        public Task<IEnumerable<OrdersHasProductContrib>> GetProductByOrderId(Guid idOrders);
+        public Task InsertProductHasOrder(OrdersHasProductContrib ordersHasProduct);
+        public Task UpdateProductHasOrder(OrdersHasProductContrib ordersHasProduct);
+        public Task DeleteOrderFromOrdersHasProduct(Guid idOrders);
+        public Task DeleteProductFromOrdersHasProduct(Guid idOrders, int idProduct);
+    }
+}
