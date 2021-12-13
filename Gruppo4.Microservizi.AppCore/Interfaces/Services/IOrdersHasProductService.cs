99fd2f60ff788f568133a4815225cc0fbbb8f7ff
paolopiaia <83641900+paolopiaia@users.noreply.github.com>
2021-12-13 16:50:07 +0100

    Create services interfaces

---
+++ Gruppo4.Microservizi.AppCore/Interfaces/Services/IOrdersHasProductService.cs
@@ -0,0 +1,21 @@
+﻿using Gruppo4.Microservizi.AppCore.Models;
+using Gruppo4.Microservizi.AppCore.Models.ModelContrib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppo4.Microservizi.AppCore.Interfaces.Services
+{
+    public interface IOrdersHasProductService
+    {
+
+        public Task<IEnumerable<OrdersHasProductContrib>> GetProductByOrderId(Guid idOrders);
+        public Task InsertProductHasOrder(OrdersHasProductContrib ordersHasProduct);
+        public Task UpdateProductHasOrder(OrdersHasProductContrib ordersHasProduct);
+        public Task DeleteOrderFromOrdersHasProduct(Guid idOrders);
+        public Task DeleteProductFromOrdersHasProduct(Guid idOrders, int idProduct);
+
+    }
+}
