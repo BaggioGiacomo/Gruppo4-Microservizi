81137e2ae6e7fec9d426a70b19f2231b2e17f98a
paolopiaia <83641900+paolopiaia@users.noreply.github.com>
2021-12-13 16:53:49 +0100

    Add dependency injections

--- Gruppo4.Microservizi.WebApi/Program.cs
+++ Gruppo4.Microservizi.WebApi/Program.cs
@@ -17,10 +17,16 @@ builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICouponHasOrdersService, CouponHasOrdersService>();
+builder.Services.AddScoped<IOrdersHasProductService, OrdersHasProductService>();
+
+
 builder.Services.AddScoped<IProductRepository, ProductRepositoryContrib>();
 builder.Services.AddScoped<IOrderRepository, OrdersRepositoryContrib>();
 builder.Services.AddScoped<ICouponRepository, CouponsRepositoryContrib>();
 builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddScoped<ICouponHasOrdersRepository, CouponHasOrdersRepositoryContrib>();
+builder.Services.AddScoped<IOrdersHasProductRepository, OrdersHasProductRepositoryContrib>();
 
 var app = builder.Build();
 
