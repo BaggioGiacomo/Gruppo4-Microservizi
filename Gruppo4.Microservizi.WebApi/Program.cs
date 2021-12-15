dd3f21eb067091439168ab2bde2db0f3e9ece460
andreazanottoits <andrea.zanotto@stud.tecnicosuperiorekennedy.it>
2021-12-15 17:28:13 +0100

    add rabbitMQ configuration

--- Gruppo4.Microservizi.WebApi/Program.cs
+++ Gruppo4.Microservizi.WebApi/Program.cs
@@ -1,8 +1,12 @@
+using Gruppo4.Microservizi.AppCore.Consumers.Customers;
+using Gruppo4.Microservizi.AppCore.Consumers.Products;
+using Gruppo4.Microservizi.AppCore.Consumers.Warehouse;
 using Gruppo4.Microservizi.AppCore.Interfaces.Data;
 using Gruppo4.Microservizi.AppCore.Interfaces.Services;
 using Gruppo4.Microservizi.AppCore.Services;
 using Gruppo4.Microservizi.Persistency.Repositories.RepositoriesContrib;
 using Gruppo4.Microservizi.Persistency.RepositoriesContrib;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
@@ -26,6 +31,58 @@ builder.Services.AddScoped<ICustomerRepository, CustomerRepositoryContrib>();
 builder.Services.AddScoped<IProductRepository, ProductRepositoryContrib>();
 builder.Services.AddScoped<ICouponHasOrdersRepository, CouponHasOrdersRepositoryContrib>();
 builder.Services.AddScoped<IOrdersHasProductRepository, OrdersHasProductRepositoryContrib>();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, rabbitConfigurator) =>
+    {
+        rabbitConfigurator.Host(
+            "roedeer-01.rmq.cloudamqp.com",
+            "/",
+            credentials =>
+            {
+                credentials.Username("vpeeygzh");
+                credentials.Password("t0mDd3KRsJkXRV3DXzmCUfRWmDFbFu42");
+            });
+
+        rabbitConfigurator.ReceiveEndpoint("test_1", e =>
+        {
+            e.Consumer<CreateClientEventConsumer>();
+        });
+        rabbitConfigurator.ReceiveEndpoint("test_2", e =>
+        {
+            e.Consumer<DeleteClientEventConsumer>();
+        });
+
+        rabbitConfigurator.ReceiveEndpoint("test_2", e =>
+        {
+            e.Consumer<UpdateClientEventConsumer>();
+        });
+
+        rabbitConfigurator.ReceiveEndpoint("test_2", e =>
+        {
+            e.Consumer<DeleteProductEventConsumer>();
+        });
+
+        rabbitConfigurator.ReceiveEndpoint("test_2", e =>
+        {
+            e.Consumer<NewProductEventConsumer>();
+        });
+
+        rabbitConfigurator.ReceiveEndpoint("test_2", e =>
+        {
+            e.Consumer<UpdateProductEventConsumer>();
+        });
+
+        rabbitConfigurator.ReceiveEndpoint("test_2", e =>
+        {
+            e.Consumer<NewRefillEventConsumer>();
+        });
+
+    });
+
+});
+builder.Services.AddMassTransitHostedService();
 
 var app = builder.Build();
 
